c50acef2184211e448b930664fede63db21b2268
wsqtql <51528785+wsqtql@users.noreply.github.com>
2022-04-30 19:56:25 +0800

    Update rela.py

--- rela.py
+++ rela.py
@@ -5,33 +5,33 @@ import math
 class Rela:
     def rela(X):
         #Find the required number of nearest neighbors k
-        a1 = X.shape[0]
-        b1 = math.pow(a1, 0.5)
-        k = math.ceil(b1)
+        shape_x = X.shape[0]
+        pow_shape = math.pow(shape_x, 0.5)
+        k = math.ceil(pow_shape)
         neigh = NearestNeighbors(n_neighbors=k)
         neigh.fit(X)
         neigh1 = neigh.kneighbors(X, return_distance=True)
         a, b = neigh1
         # a represents the distance of k-nearest neighbor samples, b is the index of k-nearest neighbor samples
         
-        c = []
+        avg_dis = []
         for i_a in a:
-            c.append((sum(i_a)+0.0001) / (len(i_a) - 1))
-        #  c represents the average of the distances of the k nearest neighbors of all points
-        d = []
-        for i_c in c:
-            d.append(1 / i_c)
-        # d represents the reciprocal of the mean distance
-        e = []
-        for i_d in d:
-            e.append(i_d/sum(d))
-        # e represents the proportion of each term of d in the sum, representing the relative density of the sample
+            avg_dis.append((sum(i_a)+0.0001) / (len(i_a) - 1))
+        #  avg_dis represents the average of the distances of the k nearest neighbors of all points
+        mean = []
+        for i_avg in avg_dis:
+            mean.append(1 / i_avg)
+        # mean represents the reciprocal of the mean distance
+        rela_density = []
+        for i_mean in mean:
+            rela_density.append(i_mean/sum(mean))
+        # rela_density represents the proportion of each term of d in the sum, representing the relative density of the sample
 
-        arr_e = np.array(e)
-        arr_c = np.array(c)
+        arr_density = np.array(rela_density)
+        arr_dis = np.array(avg_dis)
         #arr_cc is a cost-sensitive parameter
-        arr_cc = np.sqrt(np.abs(arr_c))
-        list_f = list(np.true_divide(arr_e,arr_cc))
+        cost = np.sqrt(np.abs(arr_dis))
+        list_f = list(np.true_divide(arr_density,cost))
         return list_f
 
     
