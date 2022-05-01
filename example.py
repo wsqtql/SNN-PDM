b13f2f209e65075bb288668c728e7341597becd8
wsqtql <51528785+wsqtql@users.noreply.github.com>
2022-05-01 13:42:08 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,3 @@
 from predict import Predict
-#You can obtain predictions by feeding the processed dataset into the following function   
+#You can obtain predictions by feeding the processed dataset into the following function，and the training data is in numpy format。 
 y_predict=Predict.predict(X_test,X_train,y_train)
